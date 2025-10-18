d4e9abbeed6e12f42465044536ea54f6bc94c0d3
jay1535 <habbujayanth@gmail.com>
2025-10-18 17:59:40 +0530

    Added Objests

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -1,19 +1,16 @@
 /*
 Apart from primitives, the most common sort of type you’ll encounter is an object type.
- This refers to any JavaScript value with properties, which is almost all of them! 
+ This refers to any JavaScript value with properties, which is almost all of them!
  To define an object type, we simply list its properties and their types.
 
 For example, here’s a function that takes a point-like object:
 */
-
 // The parameter's type annotation is an object type
-function printCoord(pt: { x: number; y: number }) {
-  console.log("The coordinate's x value is " + pt.x);
-  console.log("The coordinate's y value is " + pt.y);
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
 }
 printCoord({ x: 3, y: 7 });
-
-
 /*
 
 Here, we annotated the parameter with a type with two properties - x and y - which are both of type number.
@@ -26,18 +23,15 @@ Object types can also specify that some or all of their properties are optional.
  To do this, add a ? after the property name:
 
 */
-
-
-function printName(obj: { first: string; last?: string }) {
-  // ...
+function printName(obj) {
+    // ...
 }
 // Both OK
 printName({ first: "Bob" });
 printName({ first: "Alice", last: "Alisson" });
-
 /*
-In JavaScript, if you access a property that doesn’t exist, 
-you’ll get the value undefined rather than a runtime error. 
-Because of this, when you read from an optional property, 
+In JavaScript, if you access a property that doesn’t exist,
+you’ll get the value undefined rather than a runtime error.
+Because of this, when you read from an optional property,
 you’ll have to check for undefined before using it.
-*/
+*/ 
