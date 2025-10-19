261557bb78ed7cb405b2d61bf7daeac8aa59c454
jay1535 <habbujayanth@gmail.com>
2025-10-19 15:21:35 +0530

    Added syntax for Arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,8 @@
+/*
+To specify the type of an array like [1, 2, 3], you can use the syntax number[];
+this syntax works for any type (e.g. string[] is an array of strings, and so on).
+ You may also see this written as Array<number>, which means the same thing.
+ We’ll learn more about the syntax T<U> when we cover generics.
+*/
+var superHeroes = [];
+superHeroes.push("Spiderman");
