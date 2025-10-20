17492e0162a3823ec2372cc537edc1b691a472e8
jay1535 <habbujayanth@gmail.com>
2025-10-20 20:10:45 +0530

    Added Tuples Folder

---
+++ Tuples/Tuples.js
@@ -0,0 +1,6 @@
+"use strict";
+//To maintain the order of the array. 
+// Helpful in API calls  or to maintain the order of data in a database.
+Object.defineProperty(exports, "__esModule", { value: true });
+var tupleUser;
+tupleUser = ["Jayant", 1, true];
---
+++ Tuples/Tuples.ts
@@ -0,0 +1,7 @@
+//To maintain the order of the array. 
+// Helpful in API calls  or to maintain the order of data in a database.
+
+let tupleUser : [string , number, boolean]
+tupleUser = ["Jayant", 1, true]
+
+export {}
