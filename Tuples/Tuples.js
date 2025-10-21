e5dc9aaae6412ffc16e37cb709368c4203f4be50
jay1535 <habbujayanth@gmail.com>
2025-10-21 09:03:15 +0530

    Added User Types in tuples

--- Tuples/Tuples.js
+++ Tuples/Tuples.js
@@ -1,6 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //To maintain the order of the array. 
 // Helpful in API calls  or to maintain the order of data in a database.
-Object.defineProperty(exports, "__esModule", { value: true });
 var tupleUser;
 tupleUser = ["Jayant", 1, true];
+var newUser = [123, "NewUser"];
+newUser[1] = "hc.com";
--- Tuples/Tuples.ts
+++ Tuples/Tuples.ts
@@ -1,7 +1,11 @@
 //To maintain the order of the array. 
 // Helpful in API calls  or to maintain the order of data in a database.
-
 let tupleUser : [string , number, boolean]
 tupleUser = ["Jayant", 1, true]
+
+type Users = [number, string]
+const newUser : Users = [123, "NewUser"]
+newUser[1] = "hc.com"
+
 
 export {}
