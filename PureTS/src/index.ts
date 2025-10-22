e53330580abf7556b335ac83e1eb8a89a78588d0
jay1535 <habbujayanth@gmail.com>
2025-10-22 13:13:43 +0530

    Completed getter and setter

--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -10,14 +10,27 @@
 
 // const jayant = new User("Jayant@gmail.com", "Jayant");
 class User{
-    
+    private _courseCount = 1
     private readonly city : string = ""
     constructor(
         public email : string,
          public name : string,
         // private userId : string
-    ){
-        
+    ){}
+
+    get getAppleEmail ():string{ //Use get keyword
+        return `apple ${this.email}`;
+    }
+    get courseCount (): number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if(courseNum<=1){
+            throw new Error("Course count should be greater than 1")
+        }
+        this._courseCount = courseNum;
+
     }
 };
 
