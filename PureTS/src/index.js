dc0d6fd81ca235356ff6178a1d11bff2915c96b4
jay1535 <habbujayanth@gmail.com>
2025-10-22 09:33:11 +0530

    Classes and constructors in TS

--- PureTS/src/index.js
+++ PureTS/src/index.js
@@ -1,1 +1,11 @@
-console.log("Hey This is fun");
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+;
+var jayant = new User("Jayant@gmail.com", "Jayant");
+jayant.city = "Jaipur";
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hey This is fun");
+class User{
+    email:string
+    name:string
+    city : string = ""
+    constructor(email : string, name : string){
+        this.email = email;
+        this.name = name;
+    }
+};
+
+const jayant = new User("Jayant@gmail.com", "Jayant");
+jayant.city = "Jaipur";
