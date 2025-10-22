c4ec964269b91250af75e57854ef17eb95f4d686
jay1535 <habbujayanth@gmail.com>
2025-10-22 13:05:18 +0530

    Added access modifiers

--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,12 +1,25 @@
+// class User{
+//     email:string
+//     name:string
+//     private readonly city : string = ""
+//     constructor(email : string, name : string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// };
+
+// const jayant = new User("Jayant@gmail.com", "Jayant");
 class User{
-    email:string
-    name:string
-    city : string = ""
-    constructor(email : string, name : string){
-        this.email = email;
-        this.name = name;
+    
+    private readonly city : string = ""
+    constructor(
+        public email : string,
+         public name : string,
+        // private userId : string
+    ){
+        
     }
 };
 
 const jayant = new User("Jayant@gmail.com", "Jayant");
-jayant.city = "Jaipur";
+// jayant.city = "Jaipur"; Cannot Access ---> Priavte
