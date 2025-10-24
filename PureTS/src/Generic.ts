1b9677a2ac9d926af588cf7716e086150e2dbcdc
jay1535 <habbujayanth@gmail.com>
2025-10-24 15:26:35 +0530

    Use of Class Type and Parameter type in Generics

--- PureTS/src/Generic.ts
+++ PureTS/src/Generic.ts
@@ -30,3 +30,37 @@ const getMoreSearchProducts = <T,>(products : T[]) : T | undefined => {
   const myIndex = 4;
   return products[myIndex]
 }
+
+//Use of Parameter types in Generics
+
+interface Database{
+    connection : string,
+    password : string,
+    network : string
+}
+
+function anotherFunction <T,U extends Database>(valOne:T, valTwo : U): object{
+return{
+    valOne,
+    valTwo
+}
+}
+
+// anotherFunction(3, 4)
+
+
+//Use of class Type in Generics
+
+
+interface Quiz{
+    name : string,
+    question : string,
+    ans : string
+}
+
+class Sellable<T>{
+    public cart : T[] = [];
+    addToCart(product : T){
+        this.cart.push(product)
+    }
+}
